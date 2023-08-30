f98adbb4ba2a69cba4927944898aa9ffd403b415
Heidy Beltran <HeidyBeltran@Alains-MacBook-Air.local>
2023-08-30 18:37:48 -0400

    added last folder 'dist' to the code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// TODO: Added logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log("PUT to the database");
   const todosDb = await openDB("jate", 1);
@@ -23,7 +23,7 @@ export const putDb = async (content) => {
   console.log("🚀 - data saved to the database", result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+// TODO: Added logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log("GET from the database");
   const textDb = await openDB("jate", 1);
